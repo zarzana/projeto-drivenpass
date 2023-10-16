af8b7f0bf88d348e2947ff6e57a012a6c9d67267
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-10-16 07:10:57 -0300

    feat: cross-env setup

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 
 export function loadEnv() {
-  const path = '.env';
-  dotenv.config({ path });
+  const path =
+    process.env.NODE_ENV === 'test'
+      ? '.env.test'
+      : process.env.NODE_ENV === 'development'
+      ? '.env.development'
+      : '.env';
+
+  const currentEnvs = dotenv.config({ path });
+  dotenvExpand.expand(currentEnvs);
 }
